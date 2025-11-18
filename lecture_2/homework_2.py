71544766e6dfb3b397905a82ee581e2b189e126c
Maria Lobachevskaya <lobachevskayamaria@gmail.com>
2025-11-18 21:43:17 +0300

    Добавлено первое задание

---
+++ lecture_2/homework_2.py
@@ -0,0 +1,5 @@
+print("Hello!Welcome to the program")
+full_name = input("Please enter your full name:")
+birth_year = int(input("What year were you born?"))
+age = 2025-birth_year
+print("You are", age, "years old.")
